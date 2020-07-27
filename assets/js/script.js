c1ecd4d7dadc31d2ffe15cfbc06d92f3b99dcd41
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-07-27 16:36:47 -0400

    what I got so far

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,36 @@
 // document ready
 $(function() {
 
-  let citySearchName = "";
+  const citySearchForm = $("#searchForm");
+  const citySearchName = $("#citySearch");
+  const searchedCityList = $("#cityList")
   let cityHistory = [];
-  
+
+  function renderCities() {
+    // first clear elements
+    searchedCityList.innerHTML = "";
+    // create new li for each cityHistory
+    // cityHistory.length loop
+  }
+
+  function storeCities() {
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+  }
+
+  // when search city name is entered... 
+  $(citySearchForm).on("submit", function() {
+    event.preventDefault();
+    const nameToSearch = citySearchName.value.trim();
+    if (nameToSearch === "") {
+      return;
+    }
+    cityHistory.push(nameToSearch);
+    // clear search field
+    citySearchName.value = "";  
+    console.log(citySearchName);
+    storeCities();
+    renderCities();
+  });
+
 
 });
